2a6eec56b6b10937a5042d7ed9568b44234d2ff9
jordan <jojohersh@gmail.com>
2023-01-05 19:31:45 -0500

    created admin entity

--- GroupProject.Data/Entities/Admin.cs
+++ GroupProject.Data/Entities/Admin.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,11 @@ namespace GroupProject.Data.Entities
 {
     public class Admin
     {
-        
+        [Key]
+        public int Id {get; set;}
+        [Required]
+        public string UserName {get;set;}
+        [Required]
+        public string Password {get; set;}
     }
 }
