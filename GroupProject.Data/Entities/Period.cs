bf9adbc5fd91530cec82b6bb6c94b2f33520a208
Brody Hinton <brodyhinton@gmail.com>
2023-01-05 19:54:15 -0500

    period entity

--- GroupProject.Data/Entities/Period.cs
+++ GroupProject.Data/Entities/Period.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,15 @@ namespace GroupProject.Data.Entities
 {
     public class Period
     {
-        
+        [Key]
+        public int Id {get; set;}
+
+        [Required]
+        public string Name {get; set;}
+
+        [Required]
+        public DateOnly StartYear {get; set;}
+
+        public DateOnly? EndYear {get; set;}
     }
 }
