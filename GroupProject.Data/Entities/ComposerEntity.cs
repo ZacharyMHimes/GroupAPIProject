82d7b3ba1c82ab7b276381451be7e01937c56c63
jordan <jojohersh@gmail.com>
2023-01-05 20:23:20 -0500

    made composer entity

--- GroupProject.Data/Entities/ComposerEntity.cs
+++ GroupProject.Data/Entities/ComposerEntity.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,21 @@ namespace GroupProject.Data.Entities
 {
     public class ComposerEntity
     {
-        
+        [Key]
+        public int Id {get; set;}
+        [Required]
+        public string FirstName {get;set;}
+        [Required]
+        public string LastName {get; set;}
+        public string? Nationality {get; set;}
+        public DateOnly? BirthDate {get; set;}
+        public DateOnly? DeathDate {get; set;}
+        [Required]
+        public int CauseOfDeathId {get; set;}
+        public int? SexyQuotientUpVotes {get;set;} = 0;
+        public int? SexyQuotientTotalVotes {get; set;} = 0;
+
+        public CauseOfDeathEntity CauseOfDeath {get; set;}
+        public List<CompositionEntity> Compositions {get; set;}
     }
 }
