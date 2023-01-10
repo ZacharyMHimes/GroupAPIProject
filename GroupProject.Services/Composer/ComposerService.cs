bfc723a685dc6f2d5e8d7eef06038e595f0a750e
Zachary Himes <zacharymhimes@gmail.com>
2023-01-09 22:13:58 -0500

    Program.cd lines 12-16. AddEndpointApiExplorer, and Get Connection string (is this redundant?

--- GroupProject.Services/Composer/ComposerService.cs
+++ GroupProject.Services/Composer/ComposerService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using GroupProject.Data;
+using GroupProject.Models.Composer;
 
 namespace GroupProject.Services.Composer 
 {
