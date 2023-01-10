2967dd08286ead2f2fe1f6a58d5c52ede49b69c9
Zachary Himes <zacharymhimes@gmail.com>
2023-01-09 23:12:49 -0500

    Composer Create View Model

--- GroupProject.Models/Composer/ComposerCreate.cs
+++ GroupProject.Models/Composer/ComposerCreate.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,21 +8,27 @@ namespace GroupProject.Models.Composer
 {
     public class ComposerCreate
     {
-        
-        public int Id {get; set;}
-
+        //ID will be assigned by DB
+        [Required]
+        [MinLength
+            (2, ErrorMessage = "{0} must be at least {1} characters long.")]
+        [MaxLength
+            (100, ErrorMessage = "{0} must contain no more than {1} characters.")]
         public string FirstName {get;set;}
-
+        [Required]
+        [MinLength
+            (2, ErrorMessage = "{0} must be at least {1} characters long.")]
+        [MaxLength
+            (100, ErrorMessage = "{0} must contain no more than {1} characters.")]
         public string LastName {get; set;}
         public string? Nationality {get; set;}
         public DateOnly? BirthDate {get; set;}
         public DateOnly? DeathDate {get; set;}
 
-        public int CauseOfDeathId {get; set;}
+        //ComposerEntity Will inherit Cause of Death from Cause of Death create MVC
         public int? SexyQuotientUpVotes {get;set;} = 0;
         public int? SexyQuotientTotalVotes {get; set;} = 0;
 
-        public CauseOfDeathEntity CauseOfDeath {get; set;}
-        public List<CompositionEntity> Compositions {get; set;}
+        //Composer will inherit Compositions from Composition MVC
     }
 }
---
+++ GroupProject.Models/Composer/ComposerListItem.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GroupProject.Models.Composer
+{
+    public class ComposerListItem
+    {
+        public int Id {get; set;}
+        public string FirstName {get; set;}
+        public string LastName {get; set;}
+        
+    }
+}
