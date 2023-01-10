5342a92ad1690ca0d26486bb78a0d04e686982f7
Zachary Himes <zacharymhimes@gmail.com>
2023-01-10 00:13:54 -0500

    ComposerUpdate and Delete Controller Methods

--- GroupProject.WebAPI/Controllers/ComposerController.cs
+++ GroupProject.WebAPI/Controllers/ComposerController.cs
@@ -36,6 +36,34 @@ namespace GroupProject.WebAPI.Controllers
                 var notes = await _composerService.GetAllComposersAsync();
                 return Ok();
             }
+
+        [HttpGet("{noteId:int}")]
+        public async Task<IActionResult> GetComposerById([FromRoute] int composerId)
+        {
+            var detail = await _composerService.GetComposerIdAsync(composerId);
+            return detail is not null
+                ? Ok(detail)
+                : NotFound();
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateComposerById([FromBody] ComposerUpdate request)
+        {
+            if(!ModelState.IsValid)
+                return BadRequest(ModelState);
+            
+            return await _composerService.UpdateComposerAsync(request)
+                ? Ok("Composer entry updated successfully.")
+                : BadRequest("Composer entry could not be updated.");
+        }
+        
+        [HttpDelete("{noteId:int}")]
+        public async Task<IActionResult> DeleteComposer([FromRoute] int composerId)
+        {
+            return await _composerService.DeleteComposerAsync(composerId)
+                ? Ok($"Composer Entry {composerId} was deleted successfully.")
+                : BadRequest($"Composer Entry {composerId} could not be deleted.");
+        }
         
     }
 }
