f161c6b241dfb1ecfb8d6e474b9554e3296a0e83
Brody Hinton <brodyhinton@gmail.com>
2023-01-09 23:42:54 -0500

    genre models

---
+++ GroupProject.Models/Genre/GenreListItem.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GroupProject.Models.Genre
+{
+    public class GenreListItem
+    {
+        public int Id {get; set;}
+        public string GenreName {get; set;}
+    }
+}
