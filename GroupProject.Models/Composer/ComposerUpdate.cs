161028e403596ae2304ce0733aea0970d16c2696
Zachary Himes <zacharymhimes@gmail.com>
2023-01-09 23:18:19 -0500

    Composer Update View Model

---
+++ GroupProject.Models/Composer/ComposerUpdate.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GroupProject.Models.Composer
+{
+    public class ComposerUpdate
+    {
+        public int Id {get; set;}
+        public string? FirstName {get; set;}
+        public string? LastName {get; set;}
+        public string? Nationality {get; set;}
+        public DateOnly? BirthDate {get; set;}
+        public DateOnly? DeathDate {get; set;}
+        
+        public int? SexyQuotientUpVotes {get;set;}
+        public int? SexyQuotientTotalVotes {get; set;}
+    }
+}
