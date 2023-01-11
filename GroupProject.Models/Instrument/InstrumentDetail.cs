7b4623562cf926f8ea21e617e479921998e9ca16
Brody Hinton <brodyhinton@gmail.com>
2023-01-10 19:02:25 -0500

    instrument models

---
+++ GroupProject.Models/Instrument/InstrumentDetail.cs
@@ -0,0 +1,14 @@
+using System.Globalization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GroupProject.Models.Instrument
+{
+    public class InstrumentDetail
+    {
+        public int Id {get; set;}
+        public string InstrumentName {get; set;}
+    }
+}
