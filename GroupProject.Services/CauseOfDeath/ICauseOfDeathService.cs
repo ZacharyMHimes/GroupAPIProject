db93cca243635ad5f6f8dcff3a74aff2ab58f784
Zachary Himes <zacharymhimes@gmail.com>
2023-01-10 19:14:00 -0500

    Interface service method calls

--- GroupProject.Services/CauseOfDeath/ICauseOfDeathService.cs
+++ GroupProject.Services/CauseOfDeath/ICauseOfDeathService.cs
@@ -7,6 +7,11 @@ namespace GroupProject.Services.CauseOfDeath
 {
     public interface ICauseOfDeathService
     {
-        
+    Task<bool> CreateCauseOfDeathAsync(CauseCreate request);
+    Task<IEnumerable<CauseListItem>> GetAllCausesAsync(CauseListItem request);
+    Task<CauseModel> GetCauseById(int causeId);
+    Task<CauseModel> UpdateCause(int causeId);
+    Task<bool> DeleteCause(int causeId);
+
     }
 }
