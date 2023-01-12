d5847ec1b9b102204b9bab94f1b94dfa06a2cc49
Zachary Himes <zacharymhimes@gmail.com>
2023-01-11 20:38:27 -0500

    set up Composition epic

---
+++ GroupProject.WebAPI/Controllers/CompositionController.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GroupProject.Services.Composition;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GroupProject.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CompositionController : ControllerBase
+    {
+        private readonly ICompositionService _compositionService;
+        public CompositionController(ICompositionService compositionService )
+        {
+            _compositionService = compositionService;
+        }
+        
+    }
+}
