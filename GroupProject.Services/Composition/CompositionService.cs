cf8eaebaffb0666b4bb7f8e9e88442a0a67c4351
Zachary Himes <zacharymhimes@gmail.com>
2023-01-11 21:23:49 -0500

    Update and Delete async methods and model

--- GroupProject.Services/Composition/CompositionService.cs
+++ GroupProject.Services/Composition/CompositionService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using GroupProject.Data;
+using GroupProject.Models.Composition;
 
 namespace GroupProject.Services.Composition
 {
@@ -12,6 +13,29 @@ namespace GroupProject.Services.Composition
         public CompositionService(ApplicationDbContext dbContext)
         {
             _dbContext = dbContext;
+        }
+
+
+
+
+        public async Task<bool> UpdateCompositionAsync(CompositionUpdate request)
+        {
+            var compositionEntity = await _dbContext.Compositions.FindAsync(request.Id);
+            compositionEntity.Id = request.Id;
+            compositionEntity.Title = request.Title;
+            compositionEntity.OpusNumber = request.OpusNumber;
+            compositionEntity.TotalViews = request.TotalViews;
+            compositionEntity.DitterDorfs = request.DitterDorfs;
+
+            var numberOfChanges = await _dbContext.SaveChangesAsync();
+            return numberOfChanges == 1;
+
+        }
+        public async Task<bool> DeleteCompositionAsync(int Id)
+        {
+            var compositionEntity = await _dbContext.Composers.FindAsync(Id);
+            _dbContext.Composers.Remove(compositionEntity);
+            return await _dbContext.SaveChangesAsync() == 1;
         }
     }
 }
