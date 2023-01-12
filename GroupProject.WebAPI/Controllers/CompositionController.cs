86552e636e4514d60370f8c5ef73463fff5ecfeb
Zachary Himes <zacharymhimes@gmail.com>
2023-01-12 17:15:22 -0500

    added Delete and Update endpoints

--- GroupProject.WebAPI/Controllers/CompositionController.cs
+++ GroupProject.WebAPI/Controllers/CompositionController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GroupProject.Models.Composition;
 using GroupProject.Services.Composition;
 using Microsoft.AspNetCore.Mvc;
 
@@ -17,5 +18,23 @@ namespace GroupProject.WebAPI.Controllers
             _compositionService = compositionService;
         }
         
-    }
+        [HttpPut]
+        public async Task<IActionResult> UpdateComposerById([FromBody] CompositionUpdate request)
+        {
+            if(!ModelState.IsValid)
+                return BadRequest(ModelState);
+            
+            return await _compositionService.UpdateCompositionAsync(request)
+                ? Ok("Composer entry updated successfully.")
+                : BadRequest("Composer entry could not be updated.");
+        }
+        
+        [HttpDelete("{Id:int} {firstName:string} {lastName:string}")]
+        public async Task<IActionResult> DeleteComposer([FromRoute] int Id, string Title)
+        {
+            return await _compositionService.DeleteCompositionAsync(Id)
+                ? Ok($"Composition: {Title} was deleted successfully.") 
+                : BadRequest($"Composition entry: {Title} could not be deleted.");
+        }
+        }
 }
