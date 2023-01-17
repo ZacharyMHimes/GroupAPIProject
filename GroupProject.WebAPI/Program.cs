f9a5b0bd3af0e2ad2606fea862fd701376608ae4
jordan <jojohersh@gmail.com>
2023-01-17 18:10:05 -0500

    reset connection string to defaultConnection

--- GroupProject.WebAPI/Program.cs
+++ GroupProject.WebAPI/Program.cs
@@ -11,7 +11,7 @@ using GroupProject.Services.Instrumentation;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("TestConnection");
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
