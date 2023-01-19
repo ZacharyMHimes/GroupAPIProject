d575d4141359fec92e2473fe64abe6b7b6de13c0
Brody Hinton <brodyhinton@gmail.com>
2023-01-18 20:22:00 -0500

    update tags

--- GroupProject.Models/Composer/ComposerUpdate.cs
+++ GroupProject.Models/Composer/ComposerUpdate.cs
@@ -8,8 +8,17 @@ namespace GroupProject.Models.Composer
 {
     public class ComposerUpdate
     {
+        [Required]
         public int Id {get; set;}
+         [MinLength
+            (2, ErrorMessage = "{0} must be at least {1} characters long.")]
+        [MaxLength
+            (100, ErrorMessage = "{0} must contain no more than {1} characters.")]
         public string? FirstName {get; set;}
+         [MinLength
+            (2, ErrorMessage = "{0} must be at least {1} characters long.")]
+        [MaxLength
+            (100, ErrorMessage = "{0} must contain no more than {1} characters.")]
         public string? LastName {get; set;}
         public string? Nationality {get; set;}
         public DateTime? BirthDate {get; set;}
